18959ff3105b3c073338d5b3a26f86bd2e38ba46
isahVrosales <isah.rosales@colorado.edu>
2025-04-08 21:29:38 -0600

    11

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -5,15 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declare an array of image filenames */
-const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imageFilenames = ['Isah1.jpg', 'Isah2.jpg', 'Isah3.jpg', 'Isah4.jpg', 'Isah5.jpg'];
 
 /* Declare an object with alternative text for each image */
 const altText = {
-  'pic1.jpg': 'Closeup of a human eye',
-  'pic2.jpg': 'Rock that looks like a wave',
-  'pic3.jpg': 'Purple and white pansies',
-  'pic4.jpg': 'Section of wall from a pharaoh’s tomb',
-  'pic5.jpg': 'Large moth on a leaf'
+  'Isah1.jpg': 'Closeup of a human eye',
+  'Isah2.jpg': 'Rock that looks like a wave',
+  'Isah3.jpg': 'Purple and white pansies',
+  'Isah4.jpg': 'Section of wall from a pharaoh’s tomb',
+  'Isah5.jpg': 'Large moth on a leaf'
 };
 
 /* Looping through images */
